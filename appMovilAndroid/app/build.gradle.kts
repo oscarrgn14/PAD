2ae170390c7c9a63f59b63bfe2b7dcf4e4e0f34f
oscarrgn14 <oroncero@ucm.es>
2025-05-21 21:16:10 +0200

    Pantalla Principal

--- appMovilAndroid/app/build.gradle.kts
+++ appMovilAndroid/app/build.gradle.kts
@@ -40,7 +40,13 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("androidx.room:room-runtime:$room_version")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.navigation:navigation-fragment:2.7.7")
+    implementation ("androidx.navigation:navigation-ui:2.7.7")
     annotationProcessor("androidx.room:room-compiler:$room_version")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
